4d6dcc9fbe75557dd7496018a0a9a3400af9bd34
StumboEugen <marineworld@foxmail.com>
2019-03-24 19:19:15 +0800

    the static version of average max min(TO BE DEPARTED)

--- detectLine.py
+++ detectLine.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 import os
 import random
+
 # import rospy
 # from std_msgs.msg import Float32
 
@@ -12,20 +13,35 @@ searchR = 50
 stepC = 100
 stepSize = np.pi / stepC * 2
 
+lineDevideC = 50
+
 isD = False
 
+# 1: moving on the edge
+# 2: moving to node
+# 3: aimming at node
+# 4: leaving from node
 mode = 3
 
 path = 'pics/'
+
+
+def getiDevided(i, twoP):
+    x = twoP[0][0] * i + twoP[1][0] * (1 - i)
+    y = twoP[0][1] * i + twoP[1][1] * (1 - i)
+    return [int(x), int(y)]
+
 
 if __name__ == "__main__":
     pics = os.listdir(path)
     # random.shuffle(pics)
 
     if isD:
-        pics = ['419.png']
+        pics = ['270.png']
 
     for pic in pics:
+
+        # image processing
         imgori = cv2.imread(path + pic)
         sizex = imgori.shape[1]
         sizey = imgori.shape[0]
@@ -44,7 +60,7 @@ if __name__ == "__main__":
 
         ke = np.ones((2, 2))
         img2v = cv2.erode(img2v, ke, iterations=3)
-        kd = np.ones((10, 10))
+        kd = np.ones((5, 5))
         img2v = cv2.dilate(img2v, kd)
 
         if isD:
@@ -54,31 +70,21 @@ if __name__ == "__main__":
             cv2.imwrite('after/' + pic, img2v)
 
         imgLine = cv2.Canny(img2v, 5, 200, 3)
+        if isD:
+            cv2.imshow('im', imgLine)
+            cv2.waitKey()
+        else:
+            cv2.imwrite('line/' + pic, imgLine)
 
-        lines = cv2.HoughLines(imgLine, 1, np.pi / 180, 50)
+        lines = cv2.HoughLines(imgLine, 1, np.pi / 180, 75)
 
         if lines is not None:
             lineInfos = {}
             lines1 = lines[:, 0, :]
             for pair in lines1[:]:
+
                 rho = pair[0]
                 theta = pair[1]
-
-                dirHasRecorded = False
-                for dir in lineInfos.keys():
-
-                    if abs(abs(dir - theta) - np.pi) < 0.3:
-                        theta = dir
-                        rho = -rho
-
-                    if abs(dir - theta) < 0.3:
-                        lineInfos[dir][0] = min(lineInfos[dir][0], rho)
-                        lineInfos[dir][1] = max(lineInfos[dir][1], rho)
-                        dirHasRecorded = True
-                        break
-
-                if dirHasRecorded:
-                    continue
 
                 a = np.cos(theta)
                 b = np.sin(theta)
@@ -89,7 +95,23 @@ if __name__ == "__main__":
                 y1 = int(y0 + 1000 * a)
                 x2 = int(x0 - 1000 * (-b))
                 y2 = int(y0 - 1000 * a)
-                cv2.line(imgori, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                cv2.line(imgori, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+                dirHasRecorded = False
+                for angle in lineInfos.keys():
+
+                    if abs(abs(angle - theta) - np.pi) < 0.3:
+                        theta = angle
+                        rho = -rho
+
+                    if abs(angle - theta) < 0.3:
+                        lineInfos[angle][0] = min(lineInfos[angle][0], rho)
+                        lineInfos[angle][1] = max(lineInfos[angle][1], rho)
+                        dirHasRecorded = True
+                        break
+
+                if dirHasRecorded:
+                    continue
 
                 lineInfos[theta] = [rho, rho]
 
@@ -103,6 +125,7 @@ if __name__ == "__main__":
                 cv2.imshow('im', imgori)
                 cv2.waitKey()
 
+            # one line situation
             if len(lineInfos) == 1:
 
                 a1, r1 = lineInfos.items()[0]
@@ -128,26 +151,46 @@ if __name__ == "__main__":
                 if 0 < y2 <= sizey:
                     edgePoints.append([sizex, int(y2)])
 
-                midx = (edgePoints[0][0] + edgePoints[1][0]) / 2
-                midy = (edgePoints[0][1] + edgePoints[1][1]) / 2
+                xb, yb = getiDevided(1.0 / lineDevideC, edgePoints)
+                xe, ye = getiDevided(1 - (1.0 / lineDevideC), edgePoints)
+                bIsLine = img2v[yb][xb] == 255
+                eIsLine = img2v[ye][xe] == 255
 
-                cv2.circle(imgori, (midx, midy), 10, (0, 0, 255), 5)
+                if bIsLine and eIsLine:
+                    midx = (edgePoints[0][0] + edgePoints[1][0]) / 2
+                    midy = (edgePoints[0][1] + edgePoints[1][1]) / 2
 
-                cv2.imwrite('1/' + pic, imgori)
+                    cv2.circle(imgori, (midx, midy), 10, (0, 0, 255), 5)
 
+                    cv2.imwrite('1/' + pic, imgori)
+
+                else:
+                    if bIsLine:
+                        beginType = 255
+                    else:
+                        beginType = 0
+                    for i in range(1, lineDevideC - 1):
+                        x, y = getiDevided(float(i) / lineDevideC, edgePoints)
+                        if img2v[y][x] != beginType:
+                            break
+                    cv2.circle(imgori, (x, y), 10, (0, 0, 255), 5)
+
+                    if bIsLine:
+                        cv2.line(imgori, (x, y), (xb, yb), (255, 0, 0), 5)
+                    else:
+                        cv2.line(imgori, (x, y), (xe, ye), (255, 0, 0), 5)
+
+                    cv2.imwrite('1/' + pic, imgori)
+
+            # two lines situation
             elif len(lineInfos) == 2:
                 a1, r1 = lineInfos.items()[0]
                 a2, r2 = lineInfos.items()[1]
 
-                y1 = np.sin(a1) / r1
-                x1 = np.cos(a1) / r1
-                y2 = np.sin(a2) / r2
-                x2 = np.cos(a2) / r2
-
-                ans = np.matrix([[x1, y1], [x2, y2]]).I
+                ans = np.matrix([[np.cos(a1), np.sin(a1)], [np.cos(a2), np.sin(a2)]]).I
                 # x0 y0 is the cross point of two lines
-                x0 = int(ans[0, 0] + ans[0, 1])
-                y0 = int(ans[1, 0] + ans[1, 1])
+                x0 = int(ans[0, 0] * r1 + ans[0, 1] * r2)
+                y0 = int(ans[1, 0] * r1 + ans[1, 1] * r2)
 
                 if abs(x0 - sizex / 2) <= sizex / 4 and abs(y0 - sizey / 2) <= sizey / 4:
 
@@ -195,15 +238,12 @@ if __name__ == "__main__":
                     cv2.circle(imgori, (x0, y0), 10, (0, 0, 255), 5)
                     cv2.imwrite('2/' + pic, imgori)
 
-
             else:
                 print(lineInfos)
                 cv2.imshow('im', img2v)
                 cv2.waitKey()
                 cv2.imshow('im', imgori)
                 cv2.waitKey()
-
-
 
         # cv2.imwrite('after/' + pic, imgcut)
 
@@ -212,7 +252,3 @@ if __name__ == "__main__":
         #
         # cv2.imshow('im', imgori)
         # cv2.waitKey()
-
-
-
-
