8baf2f47c0a44bc2da5e497de4e8989d3707b880
StumboEugen <marineworld@foxmail.com>
2019-03-24 15:33:04 +0800

    detect the middle of the cross by averaging the max and min of rho

--- detectLine.py
+++ detectLine.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     # random.shuffle(pics)
 
     if isD:
-        pics = ['275.png', '277.png', '278.png']
+        pics = ['419.png']
 
     for pic in pics:
         imgori = cv2.imread(path + pic)
@@ -58,24 +58,26 @@ if __name__ == "__main__":
         lines = cv2.HoughLines(imgLine, 1, np.pi / 180, 50)
 
         if lines is not None:
-            lineInfos = []
+            lineInfos = {}
             lines1 = lines[:, 0, :]
             for pair in lines1[:]:
                 rho = pair[0]
                 theta = pair[1]
 
-                blHasRecorded = False
-                for pairRecorded in lineInfos:
+                dirHasRecorded = False
+                for dir in lineInfos.keys():
 
-                    if abs(abs(pairRecorded[1] - pair[1]) - np.pi) < 0.3:
-                        blHasRecorded = True
+                    if abs(abs(dir - theta) - np.pi) < 0.3:
+                        theta = dir
+                        rho = -rho
+
+                    if abs(dir - theta) < 0.3:
+                        lineInfos[dir][0] = min(lineInfos[dir][0], rho)
+                        lineInfos[dir][1] = max(lineInfos[dir][1], rho)
+                        dirHasRecorded = True
                         break
 
-                    if abs(pairRecorded[1] - pair[1]) < 0.3:
-                        blHasRecorded = True
-                        break
-
-                if blHasRecorded:
+                if dirHasRecorded:
                     continue
 
                 a = np.cos(theta)
@@ -89,7 +91,10 @@ if __name__ == "__main__":
                 y2 = int(y0 - 1000 * a)
                 cv2.line(imgori, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
-                lineInfos.append([rho, theta])
+                lineInfos[theta] = [rho, rho]
+
+            for th in lineInfos.keys():
+                lineInfos[th] = (lineInfos[th][0] + lineInfos[th][1]) / 2
 
             if isD:
                 cv2.imshow('im', img2v)
@@ -98,107 +103,105 @@ if __name__ == "__main__":
                 cv2.imshow('im', imgori)
                 cv2.waitKey()
 
-            if not isD:
+            if len(lineInfos) == 1:
 
-                if len(lineInfos) == 1:
+                a1, r1 = lineInfos.items()[0]
+                # jie ju
+                if a1 == 0:
+                    a1 = 0.00001
+                x1 = r1 / np.cos(a1)
+                y1 = r1 / np.sin(a1)
+                x2 = x1 - sizey * np.tan(a1)
+                y2 = y1 - sizex / np.tan(a1)
 
-                    r1, a1 = lineInfos[0]
-                    # jie ju
-                    if a1 == 0:
-                        a1 = 0.00001
-                    x1 = r1 / np.cos(a1)
-                    y1 = r1 / np.sin(a1)
-                    x2 = x1 - sizey * np.tan(a1)
-                    y2 = y1 - sizex / np.tan(a1)
+                edgePoints = []
 
-                    edgePoints = []
+                if 0 < x1 <= sizex:
+                    edgePoints.append([int(x1), 0])
 
-                    if 0 < x1 <= sizex:
-                        edgePoints.append([int(x1), 0])
+                if 0 < x2 <= sizex:
+                    edgePoints.append([int(x2), sizey])
 
-                    if 0 < x2 <= sizex:
-                        edgePoints.append([int(x2), sizey])
+                if 0 <= y1 < sizey:
+                    edgePoints.append([0, int(y1)])
 
-                    if 0 <= y1 < sizey:
-                        edgePoints.append([0, int(y1)])
+                if 0 < y2 <= sizey:
+                    edgePoints.append([sizex, int(y2)])
 
-                    if 0 < y2 <= sizey:
-                        edgePoints.append([sizex, int(y2)])
+                midx = (edgePoints[0][0] + edgePoints[1][0]) / 2
+                midy = (edgePoints[0][1] + edgePoints[1][1]) / 2
 
-                    midx = (edgePoints[0][0] + edgePoints[1][0]) / 2
-                    midy = (edgePoints[0][1] + edgePoints[1][1]) / 2
+                cv2.circle(imgori, (midx, midy), 10, (0, 0, 255), 5)
 
-                    cv2.circle(imgori, (midx, midy), 10, (0, 0, 255), 5)
+                cv2.imwrite('1/' + pic, imgori)
 
-                    cv2.imwrite('1/' + pic, imgori)
+            elif len(lineInfos) == 2:
+                a1, r1 = lineInfos.items()[0]
+                a2, r2 = lineInfos.items()[1]
 
-                elif len(lineInfos) == 2:
-                    r1, a1 = lineInfos[0]
-                    r2, a2 = lineInfos[1]
+                y1 = np.sin(a1) / r1
+                x1 = np.cos(a1) / r1
+                y2 = np.sin(a2) / r2
+                x2 = np.cos(a2) / r2
 
-                    y1 = np.sin(a1) / r1
-                    x1 = np.cos(a1) / r1
-                    y2 = np.sin(a2) / r2
-                    x2 = np.cos(a2) / r2
+                ans = np.matrix([[x1, y1], [x2, y2]]).I
+                # x0 y0 is the cross point of two lines
+                x0 = int(ans[0, 0] + ans[0, 1])
+                y0 = int(ans[1, 0] + ans[1, 1])
 
-                    ans = np.matrix([[x1, y1], [x2, y2]]).I
-                    # x0 y0 is the cross point of two lines
-                    x0 = ans[0, 0] + ans[0, 1]
-                    y0 = ans[1, 0] + ans[1, 1]
+                if abs(x0 - sizex / 2) <= sizex / 4 and abs(y0 - sizey / 2) <= sizey / 4:
 
-                    if abs(x0 - sizex / 2) <= sizex / 4 and abs(y0 - sizey / 2) <= sizey / 4:
+                    thBegin = 0
+                    for i in range(0, stepC):
+                        th = i * stepSize
+                        rx = int(searchR * np.cos(th) + x0)
+                        ry = int(searchR * np.sin(th) + y0)
+                        if img2v[ry][rx] == 0:
+                            thBegin = th
+                            break
 
-                        thBegin = 0
-                        for i in range(0, stepC):
-                            th = i * stepSize
-                            rx = int(searchR * np.cos(th) + x0)
-                            ry = int(searchR * np.sin(th) + y0)
+                    ringPairs = []
+                    isInRing = False
+                    for i in range(1, stepC + 1):
+                        th = thBegin + i * stepSize
+                        rx = int(searchR * np.cos(th) + x0)
+                        ry = int(searchR * np.sin(th) + y0)
+                        if isInRing:
                             if img2v[ry][rx] == 0:
-                                thBegin = th
-                                break
+                                ringPairs[-1].append(th)
+                                isInRing = False
+                        else:
+                            if img2v[ry][rx] == 255:
+                                ringPairs.append([th])
+                                isInRing = True
 
-                        ringPairs = []
-                        isInRing = False
-                        for i in range(1, stepC + 1):
-                            th = thBegin + i * stepSize
-                            rx = int(searchR * np.cos(th) + x0)
-                            ry = int(searchR * np.sin(th) + y0)
-                            if isInRing:
-                                if img2v[ry][rx] == 0:
-                                    ringPairs[-1].append(th)
-                                    isInRing = False
-                            else:
-                                if img2v[ry][rx] == 255:
-                                    ringPairs.append([th])
-                                    isInRing = True
+                    if len(ringPairs[-1]) == 1:
+                        ringPairs[-1].append(thBegin + 2 * np.pi)
 
-                        if len(ringPairs[-1]) == 1:
-                            ringPairs[-1].append(thBegin + 2 * np.pi)
+                    if ringPairs[-1][1] < ringPairs[0][1]:
+                        ringPairs[-1][1] += 2 * np.pi
 
-                        if ringPairs[-1][1] < ringPairs[0][1]:
-                            ringPairs[-1][1] += 2 * np.pi
+                    ringDir = []
+                    for ringPair in ringPairs:
+                        th = (ringPair[0] + ringPair[1]) / 2
+                        rx = int(searchR * np.cos(th) + x0)
+                        ry = int(searchR * np.sin(th) + y0)
+                        cv2.line(imgori, (x0, y0), (rx, ry), (255, 0, 0), 3)
+                        ringDir.append(th)
 
-                        ringDir = []
-                        for ringPair in ringPairs:
-                            th = (ringPair[0] + ringPair[1]) / 2
-                            rx = int(searchR * np.cos(th) + x0)
-                            ry = int(searchR * np.sin(th) + y0)
-                            cv2.line(imgori, (x0, y0), (rx, ry), (255, 0, 0), 3)
-                            ringDir.append(th)
-
-                        cv2.imwrite('2/' + pic, imgori)
-
-                    else:
-                        cv2.circle(imgori, (x0, y0), 10, (0, 0, 255), 5)
-                        cv2.imwrite('2/' + pic, imgori)
-
+                    cv2.imwrite('2/' + pic, imgori)
 
                 else:
-                    print(lineInfos)
-                    cv2.imshow('im', img2v)
-                    cv2.waitKey()
-                    cv2.imshow('im', imgori)
-                    cv2.waitKey()
+                    cv2.circle(imgori, (x0, y0), 10, (0, 0, 255), 5)
+                    cv2.imwrite('2/' + pic, imgori)
+
+
+            else:
+                print(lineInfos)
+                cv2.imshow('im', img2v)
+                cv2.waitKey()
+                cv2.imshow('im', imgori)
+                cv2.waitKey()
 
 
 
